7c1146acecb5cb7bdc4acb02ef3377291746fbf5
Tiran03 <santiamado296@gmail.com>
2025-04-16 01:11:51 -0300

    feature/ FireBall_&&_FireTrail

---
+++ Proyecto-Final/Assets/Scripts/FireBullet.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    [Header("FireBall Settings")]
+    public float damage = 20f;
+    public float lifeTime = 3f;
+    public float speed = 10f;
+
+    private Vector2 direction;
+    private Rigidbody2D rb;
+
+    [Header("FireTrail Settings")]
+    public GameObject fireTrailPrefab;
+    public float trailSpawnRate = 0.1f;
+
+    private float trailTimer;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        if (rb.velocity.magnitude > 0.1f)
+        {
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg - 90f;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+        trailTimer += Time.deltaTime;
+        if (trailTimer >= trailSpawnRate)
+        {
+            SpawnTrail();
+            trailTimer = 0f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Plant"))
+        {
+            LifeController Health = collision.GetComponent<LifeController>();
+            if (Health != null)
+            {
+                Health.TakeDamage(damage);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetDirection(Vector2 newDirection)
+    {
+        direction = newDirection.normalized;
+        if (rb != null)
+        {
+            rb.velocity = direction * speed;
+        }
+    }
+
+
+    void SpawnTrail()
+    {
+        Instantiate(fireTrailPrefab, transform.position, Quaternion.identity);
+    }
+}
---
+++ Proyecto-Final/Assets/Scripts/FireTrail.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTrail : MonoBehaviour
+{
+    public float duration = 2f;
+    public float damagePerSecond = 10f;
+
+    private void Start()
+    {
+        Destroy(gameObject, duration);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        if (collision.CompareTag("Player") || collision.CompareTag("Plant") || collision.CompareTag("Home"))
+        {
+            LifeController life = collision.GetComponent<LifeController>();
+            if (life != null && life.IsAlive())
+            {
+                life.TakeDamage(damagePerSecond * Time.deltaTime);
+            }
+        }
+    }
+}
+
+
