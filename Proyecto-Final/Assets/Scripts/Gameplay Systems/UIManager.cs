c4c5b392d77e4c18ee12d8f7bd5349044fb55f78
Daniel Dieguez <dieguezd91@gmail.com>
2025-05-21 21:56:33 -0300

    Fix/open inventory

--- Proyecto-Final/Assets/Scripts/Gameplay Systems/UIManager.cs
+++ Proyecto-Final/Assets/Scripts/Gameplay Systems/UIManager.cs
@@ -453,7 +453,7 @@ public class UIManager : MonoBehaviour
     public void OpenInventory()
     {
         if (inventoryPanel == null || isInstructionsOpen) return;
-        if (GameManager.Instance != null && GameManager.Instance.currentGameState == GameState.Night)
+        if (GameManager.Instance != null && GameManager.Instance.currentGameState == GameState.Night || GameManager.Instance.currentGameState == GameState.Paused || GameManager.Instance.currentGameState == GameState.OnCrafting || GameManager.Instance.currentGameState == GameState.GameOver)
         {
             return;
         }
