279853d71851d8744727245bdec921d3c27c405d
Daniel Dieguez <dieguezd91@gmail.com>
2025-07-15 16:24:24 -0300

    Add weather system and update player prefab
    Introduced new weather prefabs for rain, snow, and wind, along with their meta files. Added a WeatherController script to manage weather effects. Updated the player prefab by removing the RainSplash particle system and adding new 'Clouds' and 'PlayerHand' GameObjects. Modified the GameScene to integrate the new weather system.

---
+++ Proyecto-Final/Assets/Scripts/Gameplay Systems/WeatherController.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    [Header("Weather Settings")]
+    [SerializeField] private List<ParticleSystem> dayWeatherPrefabs = new();
+
+    [SerializeField] private Transform weatherParent;
+
+    private ParticleSystem currentWeather;
+
+    void Awake()
+    {
+        if (weatherParent == null) weatherParent = this.transform;
+    }
+
+    void Start()
+    {
+        GameManager.Instance.onNewDay.AddListener(HandleNewDay);
+    }
+
+    void OnDisable()
+    {
+        GameManager.Instance.onNewDay.RemoveListener(HandleNewDay);
+    }
+
+    private void HandleNewDay(int dayIndex)
+    {
+        SpawnRandomWeather();
+    }
+
+    private void SpawnRandomWeather()
+    {
+        StopCurrentWeather();
+
+        if (dayWeatherPrefabs == null || dayWeatherPrefabs.Count == 0) return;
+
+        int idx = Random.Range(0, dayWeatherPrefabs.Count);
+
+        currentWeather = Instantiate(dayWeatherPrefabs[idx], weatherParent);
+        currentWeather.Play();
+    }
+
+    private void StopCurrentWeather()
+    {
+        if (currentWeather == null) return;
+
+        currentWeather.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        Destroy(currentWeather.gameObject, 3f);
+        currentWeather = null;
+    }
+}
