f6f11eb2cbc543bde479993630ce2824f7828599
Daniel Dieguez <dieguezd91@gmail.com>
2025-07-21 18:17:00 -0300

    Refactor world transition logic to GameManager
    Moved world transition handling from DayNightLightController and WorldTransitionAnimator to GameManager. GameManager now manages world state transitions and directly controls WorldTransitionAnimator, improving separation of concerns and centralizing game state management.

--- Proyecto-Final/Assets/WorldTransitionAnimator.cs
+++ Proyecto-Final/Assets/WorldTransitionAnimator.cs
@@ -9,55 +9,17 @@ public class WorldTransitionAnimator : MonoBehaviour
     [SerializeField] private string nightStateName = "HellMap";
     [SerializeField] private string transitionTrigger = "ChangeWorld";
 
-    [Header("Debug")]
-    [SerializeField] private bool debugMode = true;
-
     private bool isNightMode = false;
     public bool IsNightMode => isNightMode;
-    private GameState lastGameState = GameState.None;
 
     public Action<bool> OnWorldModeChanged;
 
     void Start()
     {
         if (worldAnimator == null)
-        {
             worldAnimator = GetComponent<Animator>();
-        }
-
-        if (worldAnimator == null)
-        {
-            return;
-        }
 
         SetWorldMode(false, false);
-
-        if (GameManager.Instance != null)
-        {
-            GameManager.Instance.OnGameStateChanged += OnGameStateChanged;
-            lastGameState = GameManager.Instance.currentGameState;
-        }
-    }
-
-    void OnDestroy()
-    {
-        if (GameManager.Instance != null)
-        {
-            GameManager.Instance.OnGameStateChanged -= OnGameStateChanged;
-        }
-    }
-
-    private void OnGameStateChanged(GameState newState)
-    {
-        bool shouldBeNight = newState == GameState.Night;
-        bool wasNight = lastGameState == GameState.Night;
-
-        if (shouldBeNight != wasNight)
-        {
-            TransitionToMode(shouldBeNight);
-        }
-
-        lastGameState = newState;
     }
 
     public void TransitionToDay()
@@ -72,41 +34,25 @@ public class WorldTransitionAnimator : MonoBehaviour
         TransitionToMode(true);
     }
 
+    public void ForceDayMode()
+    {
+        SetWorldMode(false, false);
+    }
+
+    public void ForceNightMode()
+    {
+        SetWorldMode(true, false);
+    }
+
     private void TransitionToMode(bool toNight)
     {
-        if (worldAnimator == null)
-        {
-            return;
-        }
+        if (worldAnimator == null) return;
 
         isNightMode = toNight;
 
         if (!string.IsNullOrEmpty(transitionTrigger))
         {
-            bool hasParameter = false;
-            foreach (AnimatorControllerParameter param in worldAnimator.parameters)
-            {
-                if (param.name == transitionTrigger && param.type == AnimatorControllerParameterType.Trigger)
-                {
-                    hasParameter = true;
-                    break;
-                }
-            }
-
-            if (hasParameter)
-            {
-                worldAnimator.SetTrigger(transitionTrigger);
-            }
-            else
-            {
-                string targetState = toNight ? nightStateName : dayStateName;
-                worldAnimator.Play(targetState);
-            }
-        }
-        else
-        {
-            string targetState = toNight ? nightStateName : dayStateName;
-            worldAnimator.Play(targetState);
+            worldAnimator.SetTrigger(transitionTrigger);
         }
 
         OnWorldModeChanged?.Invoke(isNightMode);
