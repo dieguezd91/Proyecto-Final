f198f4686df8913dd613735b9f747e26284df8bc
Daniel Dieguez <dieguezd91@gmail.com>
2025-11-19 01:25:33 -0300

    Credits

--- Proyecto-Final/Assets/Scripts/Menu/MainMenuController.cs
+++ Proyecto-Final/Assets/Scripts/Menu/MainMenuController.cs
@@ -7,12 +7,15 @@ public class MainMenuController : UIControllerBase
     [Header("Buttons")]
     [SerializeField] private ImprovedUIButton _playButton;
     [SerializeField] private ImprovedUIButton _optionsButton;
-    [SerializeField] private ImprovedUIButton _controlsButton;
+    //[SerializeField] private ImprovedUIButton _controlsButton;
     [SerializeField] private ImprovedUIButton _exitButton;
-    [SerializeField] private ImprovedUIButton _backButton;
+    [SerializeField] private ImprovedUIButton _creditsBackButton;
+    [SerializeField] private ImprovedUIButton _optionsBackButton;
+    [SerializeField] private ImprovedUIButton _creditsButton;
     
     [Header("Panels")]
     [SerializeField] private UIControllerBase _optionsPanel;
+    [SerializeField] private UIControllerBase _creditsPanel;
 
     private void Start()
     {
@@ -41,23 +44,34 @@ public class MainMenuController : UIControllerBase
             _optionsButton.OnHover.AddListener(() => { });
         }
 
-        if (_controlsButton != null)
-        {
-            _controlsButton.OnClick.AddListener(() => { });
-            _controlsButton.OnHover.AddListener(() => { });
-        }
+        // if (_controlsButton != null)
+        // {
+        //     _controlsButton.OnClick.AddListener(() => { });
+        //     _controlsButton.OnHover.AddListener(() => { });
+        // }
 
         if (_exitButton != null)
         {
             _exitButton.OnClick.AddListener(GameManager.Instance.QuitGame);
             _exitButton.OnHover.AddListener(() => { });
         }
-
         
-        if (_backButton != null)
+        if (_optionsBackButton != null)
         {
-            _backButton.OnClick.AddListener(HideOptions);
-            _backButton.OnHover.AddListener(() => { });
+            _optionsBackButton.OnClick.AddListener(HideOptions);
+            _optionsBackButton.OnHover.AddListener(() => { });
+        }
+
+        if (_creditsBackButton != null)
+        {
+            _creditsBackButton.OnClick.AddListener(HideCredits);
+            _creditsBackButton.OnHover.AddListener(() => { });
+        }
+
+        if (_creditsButton != null)
+        {
+            _creditsButton.OnClick.AddListener(ShowCredits);
+            _creditsButton.OnHover.AddListener(() => { });
         }
     }
 
@@ -80,6 +94,22 @@ public class MainMenuController : UIControllerBase
         if (_optionsPanel != null)
         {
             _optionsPanel.Hide();
+        }
+    }
+
+    private void ShowCredits()
+    {
+        if (_creditsPanel != null)
+        {
+            _creditsPanel.Show();
+        }
+    }
+
+    private void HideCredits()
+    {
+        if (_creditsPanel != null)
+        {
+            _creditsPanel.Hide();
         }
     }
 
